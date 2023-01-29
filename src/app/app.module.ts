97d2c29a47e5a0aae41d5597644be3ed9d166961
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2023-01-29 17:36:40 -0300

    Loja da Polystation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
